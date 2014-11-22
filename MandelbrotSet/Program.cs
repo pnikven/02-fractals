d8c21403a6b2f23f25383ade432d2c7d29dabd67
pnikven <pyshkinnv@gmail.com>
2014-11-22 15:53:45 +0500

    Simple Fractal - Generating in text file

---
+++ MandelbrotSet/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace MandelbrotSet
+{
+    class Program
+    {
+        private const double Step = 0.01;
+
+        static void Main(string[] args)
+        {
+            var file = new StreamWriter("result.txt",false);
+            for (double I = -2; I < 2; I+=Step)
+            {
+                for (double R = -2; R < 2; R += Step)
+                {
+                    if(MandelbrotSet.CheckPoint(R,I))
+                        file.Write('#');
+                    else 
+                        file.Write('.');
+
+                }
+                file.WriteLine("");
+            }
+            file.Close();
+        }
+    }
+
+    class MandelbrotSet
+    {
+        private const int IterationCount = 1000;
+
+
+        public static bool CheckPoint(double R, double I)
+        {
+            double cR = R, cI = I;
+            for (int i = 0; i < IterationCount; i++)
+            {
+                if (cR*cR + cI*cI > 4) return false;
+                double tR = cR*cR - cI*cI + R;
+                double tI = 2 * cR * cI + I;
+                cR = tR;
+                cI = tI;
+            }
+
+            return true;
+        }
+    }
+}
